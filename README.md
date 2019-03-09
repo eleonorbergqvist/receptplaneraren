824355027db4416e7915f5297c43df75e8eedc10
Ellie <pellair@ellies-air.lan>
2019-03-09 16:16:49 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Laravel-Docker-Scaffold
+
+This is a app scaffolder for laravel with docker support and react activated by default.
+
+## Features
+- MySQL
+- Auto migrations on startup
+- React activated by default
+- Local dev server (with `php artisan serve`)
+
+## Usage
+- Make sure you have docker installed
+- Open this repo
+- Run `docker-compose up`
+- ...Wait
+- Now open [localhost:8000](localhost:8000)
+- Done!
+
+## License
+MIT
---
+++ src/docker-entrypoint.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+CMD=$1
+
+# Wait until mysql is ready
+until nc -z db 3306; do
+    sleep 3
+done
+
+case "$CMD" in
+    "start" )
+        if [ ! -f ./.env ]; then
+            echo "No app found! Generating new..."
+            composer global require laravel/installer
+            PATH="$PATH:/root/.composer/vendor/bin"
+            laravel new laravel-project
+
+            echo "Moving project folder (this takes a little time)..."
+            cp -Rp ./laravel-project/. .
+            rm -rf ./laravel-project
+
+            echo "Activating react"
+            php artisan preset react
+
+            echo "Creating database"
+            php artisan migrate:fresh
+        fi
+
+        echo "Applying migrations"
+        php artisan migrate
+
+        echo "Start server"
+        exec php artisan serve --host=0.0.0.0 --port=8000
+        ;;
+    * )
+        exec $CMD ${@:2}
+        ;;
+esac
+
