440ec991eb51572eac9c0cabd4758e08cd2ac9d6
eleonorbergqvist <eleonore295@gmail.com>
2019-03-23 09:25:04 +0100

    Add intial loggedin view

--- frontend/src/containers/StartLoggedIn/StartLoggedIn.tsx
+++ frontend/src/containers/StartLoggedIn/StartLoggedIn.tsx
@@ -1,19 +1,43 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import { Header } from "../../components/Header/Header";
+import { Footer } from "../../components/Footer/Footer";
+import { Tabs } from "../../components/Tabs/Tabs"
 
 class StartLoggedIn extends Component {
 
-  async componentDidMount() {
-    const response = await fetch('/api/ping');
-    console.log(response);
-  }
-
   render() {
     return (
-      <div>
-          <h1>StartLoggedIn</h1>
-          <Link to={`/`}>Visit StartLoggedOut page.</Link>
-      </div>
+      <React.Fragment>
+        <Header output="" />
+          <main className="container">
+            <div className="start__Container columns">
+              <div className="column">
+                <h1>Vecka 12</h1>
+                <span className="icon iconColor">
+                  <i className="fas fa-home iconColor"></i>
+                </span>
+                  <i className="fas fa-caret-right iconColor"></i>
+                  <p>Aenean iaculis gravida diam, et tincidunt diam elementum pulvinar. Curabitur 
+                    dignissim tortor at blandit iaculis. Phasellus consequat velit quis leo pharetra, 
+                    et ultricies turpis aliquet. Fusce pulvinar, leo faucibus facilisis ullamcorper, 
+                    ligula mi interdum quam, vitae accumsan leo augue sed neque.
+                    Etiam sit amet ante malesuada, luctus est at, finibus nunc. Nam feugiat feugiat 
+                    nulla ut blandit. Morbi eget porttitor mauris, sed lobortis justo. 
+                    Sed eget metus at risus laoreet consectetur bibendum a nunc. 
+                    Quisque quam magna, sollicitudin egestas orci a, congue aliquam nibh.
+                  </p>
+                <button className="button">Generate Shoppinglist</button>
+                <br />
+                <Link to={`/`}>Visit StartLoggedOut page.</Link>
+              </div>
+              <div className="column">
+                <Tabs />
+              </div>
+            </div>
+          </main>
+        <Footer copyrightText="Testtesttest"/>
+      </React.Fragment>
     );
   }
 }
