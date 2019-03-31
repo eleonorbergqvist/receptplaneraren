e4245d8fdfd82a8876c6275724bd8374afb2f7a9
eleonorbergqvist <eleonore295@gmail.com>
2019-03-31 09:52:08 +0100

    Add updated header

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -14,7 +14,7 @@ class Header extends Component<HeaderProps, HeaderState> {
     burgerIsActive: false
   }
 
-  handleBurgerMenuClick = (e:any) => {
+  handleToggleMenuClick = () => {
     this.setState({ burgerIsActive: !this.state.burgerIsActive });
   }
 
@@ -30,7 +30,7 @@ class Header extends Component<HeaderProps, HeaderState> {
             aria-label="menu"
             aria-expanded="false"
             data-target="navbarBasicExample"
-            onClick={this.handleBurgerMenuClick}
+            onClick={this.handleToggleMenuClick}
           >
             <span aria-hidden="true" />
             <span aria-hidden="true" />
