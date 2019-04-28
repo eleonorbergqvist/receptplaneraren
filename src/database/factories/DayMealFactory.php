623cea6fcbf29be5c6f37ffed08ce2461e275c89
eleonorbergqvist <eleonore295@gmail.com>
2019-04-28 17:06:29 +0200

    Update number in factory

--- src/database/factories/DayMealFactory.php
+++ src/database/factories/DayMealFactory.php
@@ -5,7 +5,7 @@ use Faker\Generator as Faker;
 $factory->define(App\DayMeal::class, function (Faker $faker) {
     return [
         'date' => $faker->date(),
-        'meal_type' => $faker->numberBetween(0, 3),
+        'meal_type' => $faker->numberBetween(0, 2),
         'user_id' => factory(App\User::class),
         'recipe_id' => factory(App\Recipe::class),
     ];
