b845209d9861e193756c95e60602514babafa53c
eleonorbergqvist <eleonore295@gmail.com>
2019-05-12 14:26:24 +0200

    Add navigation wizard for create recipe by image

--- frontend/src/components/RecipeByImageForm/RecipeByImageForm.tsx
+++ frontend/src/components/RecipeByImageForm/RecipeByImageForm.tsx
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import ReactCrop from 'react-image-crop';
 import loadImage from 'blueimp-load-image';
 import { getCroppedImg } from '../../utils/image';
-import TabNav from "../../components/TabNav/TabNav";
 import 'react-image-crop/dist/ReactCrop.css';
 import "./RecipeByImageForm.css";
 
@@ -17,17 +16,20 @@ interface CropsType {
   image: object
 }
 
+interface ImageDataType {
+  title: string
+  instructions: string
+  ingredients: string
+  image: string
+}
+
 interface RecipeByImageFormState {
   originalImageData: string,
 
   currentCrop: number,
 
   crops: CropsType,
-
-  croppedTitleImageData: any,
-  croppedInstructionsImageData: any,
-  croppedIngredientsImageData: any,
-  croppedImageImageData: any,
+  imageData: ImageDataType,
 }
 
 class RecipeByImageForm extends Component<Props, RecipeByImageFormState> {
@@ -43,10 +45,12 @@ class RecipeByImageForm extends Component<Props, RecipeByImageFormState> {
       image: {},
     },
 
-    croppedTitleImageData: '',
-    croppedInstructionsImageData: '',
-    croppedIngredientsImageData: '',
-    croppedImageImageData: '',
+    imageData: {
+      title: '',
+      ingredients: '',
+      instructions: '',
+      image: '',
+    },
   }
 
   imageRef: any;
@@ -81,31 +85,22 @@ class RecipeByImageForm extends Component<Props, RecipeByImageFormState> {
     this.imageRef = image;
   };
 
-  handleTitleCropComplete = (crop: any) => {
-    const imageData = this.getImageDataFromCrop(crop);
-    this.setState({ croppedTitleImageData: imageData });
-  };
-
-  handleInstructionsCropComplete = (crop: any) => {
-    const imageData = this.getImageDataFromCrop(crop);
-    this.setState({ croppedInstructionsImageData: imageData });
-  };
-
-  handleIngredientsCropComplete = (crop: any) => {
-    const imageData = this.getImageDataFromCrop(crop);
-    this.setState({ croppedIngredientsImageData: imageData });
-  };
-
-  handleImageCropComplete = (crop: any) => {
-    const imageData = this.getImageDataFromCrop(crop);
-    this.setState({ croppedImageImageData: imageData });
-  };
+  handleCropComplete = (name: string) => {
+    return (crop: ReactCrop.Crop) => {
+      this.setState({
+        imageData: {
+          ...this.state.imageData,
+          [name]: this.getImageDataFromCrop(crop),
+        }
+      })
+    }
+  }
 
   getImageDataFromCrop = (crop: any) => {
-    const croppedTitleImageData = getCroppedImg(
+    const croppedImageData = getCroppedImg(
       this.imageRef, crop, "newFile.jpeg"
     );
-    return croppedTitleImageData
+    return croppedImageData
   }
 
   handleCropChange = (name: string) => {
@@ -119,25 +114,31 @@ class RecipeByImageForm extends Component<Props, RecipeByImageFormState> {
   }
 
   hasCompletedCrop = (): boolean => {
-    return (
-      this.state.croppedTitleImageData &&
-      this.state.croppedInstructionsImageData &&
-      this.state.croppedIngredientsImageData &&
-      this.state.croppedImageImageData
+    return Boolean(
+      this.state.imageData.title &&
+      this.state.imageData.instructions &&
+      this.state.imageData.ingredients &&
+      this.state.imageData.image
     )
   }
 
   handleSubmit = async (values: any) => {
     this.props.onSubmit({
-      titleImageData: this.state.croppedTitleImageData,
-      instructionsImageData: this.state.croppedInstructionsImageData,
-      ingredientsImageData: this.state.croppedIngredientsImageData,
-      imageData: this.state.croppedImageImageData,
+      titleImageData: this.state.imageData.title,
+      instructionsImageData: this.state.imageData.instructions,
+      ingredientsImageData: this.state.imageData.ingredients,
+      imageData: this.state.imageData.image,
     });
   };
 
   handleCropTabNavChange = (item: any) => {
     this.setState( { currentCrop: item.value });
+  }
+
+  handleCropNextClick = () => {
+    window.scrollTo(0, 0);
+
+    this.setState( { currentCrop: this.state.currentCrop+1 });
   }
 
   render() {
@@ -172,79 +173,106 @@ class RecipeByImageForm extends Component<Props, RecipeByImageFormState> {
 
         {this.state.originalImageData &&
           <div>
-            <h3 className="title is-3">Step 2: Select the various portions of your recipe</h3>
-            <p>Select the fields that contain title, ingredients, instructions and image.</p>
-            <TabNav
-              items={[
-                {label: "Title", value: 0},
-                {label: "Instructions", value: 1},
-                {label: "Ingredients", value: 2},
-                {label: "Image", value: 3},
-              ]}
-              selected={this.state.currentCrop}
-              onChange={this.handleCropTabNavChange}
-            />
-
             <div className="is-widescreen">
               {this.state.originalImageData && this.state.currentCrop === 0 &&
-                <ReactCrop
-                  src={this.state.originalImageData}
-                  crop={this.state.crops.title}
-                  onChange={this.handleCropChange('title')}
-                  onComplete={this.handleTitleCropComplete}
-                  onImageLoaded={this.handleImageLoaded}
-                />
+                <>
+                  <h3 className="title is-3">Step 2: Select title</h3>
+                  <p>Use your mouse to draw a rectangle around the title area of your recipe</p>
+                  <ReactCrop
+                    src={this.state.originalImageData}
+                    crop={this.state.crops.title}
+                    onChange={this.handleCropChange('title')}
+                    onComplete={this.handleCropComplete('title')}
+                    onImageLoaded={this.handleImageLoaded}
+                  />
+                  {this.state.imageData.title && (
+                    <div className="RecipeByImageForm__StickyNav">
+                      <button
+                        className="RecipeByImageForm__StickyNavButton button is-primary is-inverted is-outlined"
+                        onClick={this.handleCropNextClick}>Continue to instructions</button>
+                    </div>
+                  )}
+                </>
               }
               {this.state.originalImageData && this.state.currentCrop === 1 &&
-                <ReactCrop
-                  src={this.state.originalImageData}
-                  crop={this.state.crops.instructions}
-                  onChange={this.handleCropChange('instructions')}
-                  onComplete={this.handleInstructionsCropComplete}
-                  onImageLoaded={this.handleImageLoaded}
-                />
+                <>
+                  <h3 className="title is-3">Step 3: Select instructions</h3>
+                  <p>Use your mouse to draw a rectangle around the instructions of your recipe</p>
+                  <ReactCrop
+                    src={this.state.originalImageData}
+                    crop={this.state.crops.instructions}
+                    onChange={this.handleCropChange('instructions')}
+                    onComplete={this.handleCropComplete('instructions')}
+                    onImageLoaded={this.handleImageLoaded}
+                  />
+
+                  {this.state.imageData.instructions && (
+                    <div className="RecipeByImageForm__StickyNav">
+                      <button
+                        className="RecipeByImageForm__StickyNavButton button is-primary is-inverted is-outlined"
+                        onClick={this.handleCropNextClick}>Continue to ingredients</button>
+                    </div>
+                  )}
+                </>
               }
               {this.state.originalImageData && this.state.currentCrop === 2 &&
-                <ReactCrop
-                  src={this.state.originalImageData}
-                  crop={this.state.crops.ingredients}
-                  onChange={this.handleCropChange('ingredients')}
-                  onComplete={this.handleIngredientsCropComplete}
-                  onImageLoaded={this.handleImageLoaded}
-                />
+                <>
+                  <h3 className="title is-3">Step 4: Select ingredients</h3>
+                  <p>Use your mouse to draw a rectangle around the ingredients of your recipe</p>
+                  <ReactCrop
+                    src={this.state.originalImageData}
+                    crop={this.state.crops.ingredients}
+                    onChange={this.handleCropChange('ingredients')}
+                    onComplete={this.handleCropComplete('ingredients')}
+                    onImageLoaded={this.handleImageLoaded}
+                  />
+                  {this.state.imageData.ingredients && (
+                    <div className="RecipeByImageForm__StickyNav">
+                      <button
+                        className="RecipeByImageForm__StickyNavButton button is-primary is-inverted is-outlined"
+                        onClick={this.handleCropNextClick}>Continue to image</button>
+                    </div>
+                  )}
+                </>
               }
               {this.state.originalImageData && this.state.currentCrop === 3 &&
-                <ReactCrop
-                  src={this.state.originalImageData}
-                  crop={this.state.crops.image}
-                  onChange={this.handleCropChange('image')}
-                  onComplete={this.handleImageCropComplete}
-                  onImageLoaded={this.handleImageLoaded}
-                />
+                <>
+                  <h3 className="title is-3">Step 5: Select image</h3>
+                  <p>Use your mouse to draw a rectangle around the image of your recipe</p>
+                  <ReactCrop
+                    src={this.state.originalImageData}
+                    crop={this.state.crops.image}
+                    onChange={this.handleCropChange('image')}
+                    onComplete={this.handleCropComplete('image')}
+                    onImageLoaded={this.handleImageLoaded}
+                  />
+                  <button
+                    className="button" onClick={this.handleSubmit}
+                  >I dont have a recipe image</button>
+
+                  {this.state.imageData.image && (
+                    <div className="RecipeByImageForm__StickyNav">
+                      <button
+                        className="RecipeByImageForm__StickyNavButton button is-primary is-inverted is-outlined"
+                        onClick={this.handleSubmit}>Create recipe</button>
+                    </div>
+                  )}
+                </>
               }
             </div>
 
-            {this.hasCompletedCrop() &&
-              <button
-                className="button"
-                onClick={this.handleSubmit}>Submit/Preview</button>
-            }
-            <p>Help text.</p>
-
-            {this.state.croppedTitleImageData && (
-              <img alt="Crop" style={{ maxWidth: "100%" }} src={this.state.croppedTitleImageData} />
+            {this.state.imageData.title && (
+              <img alt="Crop" style={{ maxWidth: "100%" }} src={this.state.imageData.title} />
             )}
-            {this.state.croppedInstructionsImageData && (
-              <img alt="Crop" style={{ maxWidth: "100%" }} src={this.state.croppedInstructionsImageData} />
+            {this.state.imageData.instructions && (
+              <img alt="Crop" style={{ maxWidth: "100%" }} src={this.state.imageData.instructions} />
             )}
-            {this.state.croppedIngredientsImageData && (
-              <img alt="Crop" style={{ maxWidth: "100%" }} src={this.state.croppedIngredientsImageData} />
+            {this.state.imageData.ingredients && (
+              <img alt="Crop" style={{ maxWidth: "100%" }} src={this.state.imageData.ingredients} />
             )}
-            {this.state.croppedImageImageData && (
-              <img alt="Crop" style={{ maxWidth: "100%" }} src={this.state.croppedImageImageData} />
+            {this.state.imageData.image && (
+              <img alt="Crop" style={{ maxWidth: "100%" }} src={this.state.imageData.image} />
             )}
-
-            {console.log(this.state.originalImageData)}
           </div>
         }
       </div>
